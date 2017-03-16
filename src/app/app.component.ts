32a4891880afae351de5789f070c542ae90c4998
ThejKishore <kishores1984@gmail.com>
2017-03-15 22:24:20 -0500

    adding the changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <ul>
+      <li><a routerLink="/">Home</a></li>
+       <li><a routerLink="/about">About</a></li>
+    </ul>
+    <hr />
+    <router-outlet></router-outlet>
+`,
+
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(){
+    console.log('Inside the AppComponent constructor');
+  }
 }
+
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,21 @@
 /**
  * Created by ThejKishore on 3/9/2017.
  */
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {UserComponent} from './components/user.component';
+import {AboutComponent} from './components/about.component';
+
+const appRoutes: Routes = [
+  {
+    path:'',
+    component: UserComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
